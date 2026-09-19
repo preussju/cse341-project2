b97db0eeaa134a0a0a4bdc322de39dc1d5512385
preussju <jveiga@byupathway.edu>
2026-09-19 00:50:41 -0300

    adding swagger

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
-//router.use('/', require('./swagger'));
+
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+     //#swagger.tags=['Hello World']
+    (res.send('Hello World'))
+});
+
 
 router.use('/games', require('./games'));
 router.use('/players', require('./players'));
