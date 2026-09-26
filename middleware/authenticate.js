ac5f637ec3a7b69611d727e1cb1b8b03390accd1
preussju <jveiga@byupathway.edu>
2026-09-26 14:15:11 -0300

    removing console.log

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,13 +1,7 @@
 const isAuthenticated = (req, res, next) => {
-    console.log('--- auth check on', req.method, req.originalUrl, '---');
-    console.log('session ID:', req.sessionID);
-    console.log('session contents:', req.session);
-
     if (req.session.user === undefined) {
-        console.log('BLOCKED - no session.user');
         return res.status(401).json("You do not have access");
     }
-    console.log('ALLOWED - session.user:', req.session.user);
     next();
 };
 
--- server.js
+++ server.js
@@ -57,8 +57,7 @@ passport.deserializeUser((user, done) => {
 app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
 
 app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: '/api-docs', session: false
-}),
+    failureRedirect: '/api-docs', session: false}),
     (req, res) => {
         req.session.user = req.user;
         res.redirect('/');
