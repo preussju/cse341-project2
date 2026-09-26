33d315e19f5ec33982213ae188946d46da51accf
preussju <jveiga@byupathway.edu>
2026-09-26 14:17:37 -0300

    testing host

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Games Api',
         description: 'Games Api'
     },
-    host: 'localhost:3000',
+    host: 'cse341-project2-2rud.onrender.com',
     schemes: ['http','https']
 };
 
